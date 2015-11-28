8b098091672626f08148a6f45b097e34c40c151e
Hershel Nashman <hershel.nashman@gmail.com>
2015-11-28 16:36:41 -0500

    works in console if win in three moves exactly

--- tt.js
+++ tt.js
@@ -2,6 +2,26 @@ $(document).ready(function() {
 	var winningCombos = [[1,2,3],[4,5,6]]
 	var turn = 1;
 	var xMoves = []
+	var permutation = []
+	var usedChars = []
+
+		function permutator(xMoves) {
+			var i ;
+			var ch ;
+			for(i = 0; i < input.length; i++){
+				ch = input.splice(i, 1)[0];
+				usedChars.push(ch);
+				if (input.length == 0) {
+					permutation.push(usedChars.slice());
+
+				}
+				permutator(input);
+				input.splice(i,0,ch);
+				usedChars.pop();
+			}
+
+			return permutation
+		};
 
 
 	$('td').on('click', function() {
@@ -12,7 +32,7 @@ $(document).ready(function() {
 		} else {
 			$(this).html('O');
 		}
-		checkForWinner()
+		checkForWinner(permutation)
 		$(this).off('click')
 		turn++;
 	});
@@ -27,40 +47,22 @@ $(document).ready(function() {
  // 		}
 	// };
 
-	function checkForWinner() {
-		$.each(winningCombos, function() {
-			$('#' + this[0]) == $('#' + this[1]) == "X"
-			});
-		}
-
-	var permutation = []
-	var usedChars = []
-
-	function permutator(input) {
-		var i ;
-		var ch ;
+	function checkForWinner(input) {
+		var total = 0
+		var eachTotal = []
 		for(i = 0; i < input.length; i++){
-			ch = input.splice(i, 1)[0];
-			usedChars.push(ch);
-			if (input.length == 0) {
-				permutation.push(usedChars.slice());
+		$.each(permutation[i], function() {
+			total += this;
+			//eachTotal.push(total)
+		});
+    }
+    if(total / input.length == 15)
+    	document.write("X wins")
+    return total
+	}
 
-			}
-			permutator(input);
-			input.splice(i,0,ch);
-			usedChars.pop();
-		}
 
-		return permutation
-	};
 });
-
-
-
-
-		//
-		//get every single element in the array
-		//
 
 
 
